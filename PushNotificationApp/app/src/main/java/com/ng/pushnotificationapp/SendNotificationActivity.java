842993dc2dd9cbd2ff7e12e24fcce92a06a822a7
Narottam Goyal <narottamgoyal66@gmail.com>
2020-05-03 20:39:39 +0530

    Added      PushNotificationApp/app/src/main/java/com/ng/pushnotificationapp/Api.java Modified   PushNotificationApp/app/build.gradle Modified   PushNotificationApp/app/src/main/AndroidManifest.xml Modified   PushNotificationApp/app/src/main/java/com/ng/pushnotificationapp/SendNotificationActivity.java

--- PushNotificationApp/app/src/main/java/com/ng/pushnotificationapp/SendNotificationActivity.java
+++ PushNotificationApp/app/src/main/java/com/ng/pushnotificationapp/SendNotificationActivity.java
@@ -5,6 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SendNotificationActivity extends AppCompatActivity {
 
@@ -43,5 +53,29 @@ public class SendNotificationActivity extends AppCompatActivity {
             bodyText.setError("title is empty");
             return;
         }
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://push-notification-api-server.herokuapp.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+        Call<ResponseBody> call = api.sendPushNotification(user.token, title, body);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Toast.makeText(SendNotificationActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(SendNotificationActivity.this, "Failed to send notification", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
